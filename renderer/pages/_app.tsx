96a23005e7058c8bb0e673e3f1df94e614139849
Esteban Rojas <14810250+esteban-url@users.noreply.github.com>
2023-02-18 01:54:16 -0600

    move mouse test
    Signed-off-by: Esteban Rojas <14810250+esteban-url@users.noreply.github.com>

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
+import { mouse, left, right, up, down } from "@nut-tree/nut-js";
 
+(async () => {
+    await mouse.move(left(500));
+    await mouse.move(up(500));
+    await mouse.move(right(500));
+    await mouse.move(down(500))
+})();
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
